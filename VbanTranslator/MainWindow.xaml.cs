fa0581da221efae7cef797335751f3065c1dfe58
DefinitivVair0 <heinzmutze4@gmail.com>
2026-01-16 13:35:49 +0100

    Projektdateien hinzufügen.

---
+++ VbanTranslator/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VbanTranslator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool ascii = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            L_Info.Content = "Sender must support ASCII\r\nsymbols in the form of \\12\r\nfor non-alphanumeric characters\r\nand symbols.";
+        }
+
+        private void BTN_Translate_Click(object sender, RoutedEventArgs e)
+        {
+            if (ascii && TB_SName.Text.Length != 16)
+            {
+                MessageBox.Show("Stream name has to be exactly 16 characters long for pure ASCII mode.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            List<byte> bytes = ascii ? [0x56, 0x42, 0x41, 0x4E, 0x52, 0x20, 0x20, 0x01] : [0x56, 0x42, 0x41, 0x4E, 0x52, 0x00, 0x00, 0x00];
+
+            string sName = TB_SName.Text;
+            bytes.AddRange(Encoding.ASCII.GetBytes(sName));
+
+            if (ascii) bytes.AddRange([0x20, 0x20, 0x20, 0x20]);
+            else for (int i = sName.Length; i < 20; i++) bytes.Add(0x00);
+
+            string vText = TB_VBAN.Text;
+            bytes.AddRange(Encoding.ASCII.GetBytes(vText));
+
+
+            List<char> chars = [.. Encoding.ASCII.GetString([.. bytes]).ToCharArray()];
+
+            if (!ascii)
+            {
+                int cc = chars.Count;
+
+                for (int i = 0; i < cc; i++)
+                {
+                    if (chars[i] == 0)
+                    {
+                        chars.Insert(i, '\\');
+                        i++;
+                        chars[i] = '0';
+                        chars.Insert(i, '0');
+                        i++;
+                        cc += 2;
+                    }
+                }
+            }
+
+            Clipboard.SetText(new string([.. chars]));
+
+            MessageBox.Show("Text copied to clipboard.");
+        }
+
+        private void CKB_ASCII_Checked(object sender, RoutedEventArgs e)
+        {
+            ascii = true;
+            L_Info.Content = "Stream name in VM/Matrix\r\nand this application needs to be\r\nexactly 16 characters long.";
+        }
+
+        private void CKB_ASCII_Unchecked(object sender, RoutedEventArgs e)
+        { 
+            ascii = false;
+            L_Info.Content = "Sender must support ASCII\r\nsymbols in the form of \\12\r\nfor non-alphanumeric characters\r\nand symbols.";
+        }
+    }
+}
---
+++ VbanTranslator/README.md
@@ -0,0 +1,28 @@
+﻿# VBAN Translator
+> Note that this application does not have extensive error checking/handling so when something crashes or does not output anything you should check your input.
+
+This app is designed to take in VBAN command(s) and translate it to ASCII while prepending the header.
+
+This way every application that can send text over UDP can send VBAN commands.
+
+The only limitation is that some parts of the VBAN protocoll require bytes between 0x00 and 0x19, which don't translate to any alphanumeric character or other symbol in ASCII.
+This means that the sending application has to either:
+
+- be able to read and send non-standart characters (looks something like this: )
+
+or
+
+- be able to distinguish standart and non-standart characters by utilizing \ (e.g. 0x12 can be represented as \12)
+
+If the application can do neither, it won't be possible to send VBAN messages with this method at this point in time.
+
+## "Installation"
+At the moment there is no insaller. You have to download the .zip from Releases, extract it and run the .exe inside.
+The .exe has to be run inside the folder with the other files.
+You might need to install the [.NET 10 Desktop Runtime](https://dotnet.microsoft.com/en-us/download/dotnet/10.0) (ASP .NET Core Runtime also works), if not already installed.
+
+## How to use
+1. Insert your command(s) into the "Command" text box (don't use newlines for multiple commands, write them inline and seperated by ";")
+1. Select mode using the check box
+1. Insert your VBAN-TEXT name (when using pure ASCII mode this has to be exactly 16 characters long)
+1. Click "Translate" (this will copy the output into your clipboard)
